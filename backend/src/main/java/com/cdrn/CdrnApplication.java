dd4f21604fe02b41977d3ec23cab0dfa746b7d03
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-05-05 15:22:28 +0530

    Backend Resources and Entity

---
+++ backend/src/main/java/com/cdrn/CdrnApplication.java
@@ -0,0 +1,13 @@
+package com.cdrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrnApplication.class, args);
+    }
+
+}
